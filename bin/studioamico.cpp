da0d7579c92c9d45211d20e535982fe6631530b9
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-04-05 17:00:06 +0200

    Created bin/studioamico.cpp

---
+++ bin/studioamico.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file studioamico.cpp
+ *
+ * @version 01.01 202145
+ *
+ * @brief https://training.olinfo.it/#/task/abc_studioamico/statement
+ *
+ * @ingroup studioamico
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
